3f619379ac90887001a1c2c3ce47a64c3d238f85
Dimi Racordon <dimitri.racordon@gmail.com>
2024-02-20 14:50:22 +0100

    Remove needless class property
    The width of the type is stored in the wrapped LLVM object.

--- core/src/main/scala/scalallvm/types/IntegerType.scala
+++ core/src/main/scala/scalallvm/types/IntegerType.scala
@@ -1,17 +1,12 @@
 package scalallvm
 package types
 
-/** An integer type in LLVM IR.
- *
- *  @param bitWidth The number of bits in the representation of the type's instances.
- */
-final class IntegerType private (val bitWidth: Int, val handle: LLVM.Handle) extends Type {
-
-  require(bitWidth > 0)
+/** An integer type in LLVM IR. */
+final class IntegerType private (val handle: LLVM.Handle) extends Type {
 
   /** Creates an instance with the given `bitWidth` in `context`. */
   def this(bitWidth: Int, context: Context) =
-    this(bitWidth, LLVM.IntTypeInContext(bitWidth, context.handle))
+    this(LLVM.IntTypeInContext(bitWidth, context.handle))
 
   /** Returns a constant whose LLVM IR type is `this` and whose value is `value`, truncated or
    *  sign-extended if needed to fit `bitWidth`.
